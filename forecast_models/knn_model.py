4bed0ab7041f8269ea9abe58cf3e63bedf5b3558
Dimansee <mannchoudhary04@gmail.com>
2026-03-01 00:21:35 +0530

    knn_model.py

--- forecast_models/knn_model.py
+++ forecast_models/knn_model.py
@@ -1,30 +1,16 @@
-import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.preprocessing import StandardScaler
 
-def run_knn(data):
-    data = data.copy()
-    features = ['month', 'is_weekend', 'festival_flag']
-    
-    X = data[features].fillna(0)
-    y = data['sales']
+def run_knn(df):
 
-    # KNN performs much better with scaled data
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    df = df.copy()
+    df['day'] = df['date'].dt.day
 
-    model = KNeighborsRegressor(n_neighbors=3, weights='distance')
-    model.fit(X_scaled, y)
+    X = df[['day']]
+    y = df['sales']
 
-    last_date = data['date'].max()
-    future_dates = pd.date_range(start=last_date + pd.Timedelta(days=1), periods=90)
-    future = pd.DataFrame({"date": future_dates})
-    
-    future['month'] = future['date'].dt.month
-    future['is_weekend'] = future['date'].dt.dayofweek.apply(lambda x: 1 if x >= 5 else 0)
-    future['festival_flag'] = 0
-    
-    X_future_scaled = scaler.transform(future[features])
-    future['forecast'] = model.predict(X_future_scaled)
+    model = KNeighborsRegressor(n_neighbors=3)
+    model.fit(X,y)
 
-    return future[['date', 'forecast']]
+    df['forecast'] = model.predict(X)
+
+    return df[['date','forecast']]
