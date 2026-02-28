da9d57b4fc87426c1c5c67ff3b033b4496fa6066
Dimansee <mannchoudhary04@gmail.com>
2026-02-28 20:36:18 +0530

    forecast_engine.py

--- forecast_engine.py
+++ forecast_engine.py
@@ -4,7 +4,11 @@ from forecast_models.knn_model import run_knn
 from forecast_models.prophet_model import run_prophet
 
 def get_forecast(df, model_choice, business_type):
-    if model_choice == "Moving Average": return run_moving_avg(df)
-    if model_choice == "Decision Tree": return run_decision_tree(df)
-    if model_choice == "KNN": return run_knn(df)
-    return run_prophet(df, industry=business_type)
+    if model_choice == "Moving Average": 
+        return run_moving_avg(df)
+    elif model_choice == "Decision Tree": 
+        return run_decision_tree(df)
+    elif model_choice == "KNN": 
+        return run_knn(df)
+    else: 
+        return run_prophet(df, industry=business_type)
